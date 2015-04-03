4bdb9be3d75889562c9d823e65c724ca7e190925
Justin Scott <jscott7561@gmail.com>
2015-04-03 11:03:48 -0500

    Customizing host

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -97,7 +97,20 @@ directory_name() {
   echo "%{$fg_bold[blue][%}$PROMPT_HOST: %~]%{$reset_color%}"
 }
 
-export PROMPT_HOST=$(hostname -f | cut -f 1 -d .)
+prompt_host() {
+  local host=$(hostname -f | cut -f 1 -d .)
+  
+  case "$host" in
+    previewchanges)
+      echo"demo"
+      ;;
+    *)
+      echo $host
+      ;;
+  esac
+}
+
+export PROMPT_HOST=$(prompt_host)
 export PROMPT=$'\n$(rb_prompt)$(directory_name) $(git_dirty)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
