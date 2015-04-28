29065fa24749dd09f07dd2d126a1be1f533e0b88
Justin Scott <jscott7561@gmail.com>
2015-04-28 14:56:29 -0500

    Ruby version bump.

--- ruby/chruby.zsh
+++ ruby/chruby.zsh
@@ -7,7 +7,7 @@ then
   if [[ -f "$HOME/.ruby-version" ]]; then
     ruby_version=$(head -n 1 "$HOME/.ruby-version")
   else
-    ruby_version="2.2.1"
+    ruby_version="2.2.2"
   fi
 
   chruby "$ruby_version"
