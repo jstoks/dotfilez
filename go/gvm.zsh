064977bfd90191e6c9bbdc3bc5fe8c61558b1b24
Justin Scott <jscott7561@gmail.com>
2018-02-23 16:45:08 -0600

    Updates.

---
+++ go/gvm.zsh
@@ -0,0 +1,1 @@
+source /Users/justin/.gvm/scripts/gvm
--- terminal/aliases.zsh
+++ terminal/aliases.zsh
@@ -4,3 +4,6 @@ alias sw="tmux switch -t"
 alias j="jrnl"
 alias nvim="nocorrect nvim"
 alias vi="vim"
+alias tmux='TERM=screen-256color tmux -2'
+alias tmuxinator='TERM=screen-256color tmuxinator'
+alias mux='TERM=screen-256color mux'
