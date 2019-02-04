8999c24f2c32e87ad500a3c5025e71de5d4ed581
Justin Scott <jscott7561@gmail.com>
2019-02-03 22:07:20 -0600

    Boost it.

--- ruby/ruby.zsh
+++ ruby/ruby.zsh
@@ -1,12 +1,13 @@
 
-if [[ -f "/usr/local/share/chruby/chruby.sh" ]]; then
-  source /usr/local/share/chruby/chruby.sh
-  source /usr/local/share/chruby/auto.sh
-
-  chruby_auto
-fi
-
-if [[ "$(command -v rbenv)" ]]; then
-  eval "$(rbenv init -)"
-fi
+source $HOME/.asdf/asdf.sh
+# if [[ -f "/usr/local/share/chruby/chruby.sh" ]]; then
+#   source /usr/local/share/chruby/chruby.sh
+#   source /usr/local/share/chruby/auto.sh
+#
+#   chruby_auto
+# fi
+#
+# if [[ "$(command -v rbenv)" ]]; then
+#   eval "$(rbenv init -)"
+# fi
 
--- terminal/path.zsh
+++ terminal/path.zsh
@@ -1,1 +1,2 @@
 #export PATH="$(brew --prefix)/bin:$PATH"
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
