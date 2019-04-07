7fbe51daf6618ba6fd97d03b80701a206606fabb
Justin Scott <jscott7561@gmail.com>
2019-04-07 00:11:05 -0500

    Vim config and alacritty

--- terminal/aliases.zsh
+++ terminal/aliases.zsh
@@ -1,12 +1,6 @@
-alias rpbcopy="ssh -p $CRAWLFISH_PORT localhost pbcopy"
-alias rpbpaste="ssh -p $CRAWLFISH_PORT localhost pbpaste"
 alias sw="tmux switch -t"
-alias j="jrnl"
 alias nvim="nocorrect nvim"
-alias vi="vim"
+alias vi="nvim"
 alias tmux='TERM=screen-256color tmux -2'
 alias tmuxinator='TERM=screen-256color tmuxinator'
 alias mux='TERM=screen-256color mux'
-
-
-
